821e02bf5b4db527e3d744a273e16bd7de8c7bd3
Jack Hall <jackh@groupon.com>
2014-10-30 09:33:35 -0500

    Support non-corner cases

--- app/roman_numeral.rb
+++ app/roman_numeral.rb
@@ -1,3 +1,48 @@
-def romanize(val)
-  "I"
+class Integer
+  ROMAN_NUMERALS = {
+    1000 => "M",
+    500  => "D",
+    100  => "C",
+    50   => "L",
+    10   => "X",
+    5    => "V",
+    1    => "I",
+  }
+
+  def to_roman
+    n = self
+    result = ""
+    ROMAN_NUMERALS.each do |number, letter|
+      result << letter * (n / number)
+      n = n % number
+    end
+    result
+  end
 end
+
+ROMAN_NUMERALS = {
+  1000 => "M",
+  500  => "D",
+  100  => "C",
+  50   => "L",
+  10   => "X",
+  5    => "V",
+  1    => "I",
+}
+
+def romanize(val)
+  result = ""
+  ROMAN_NUMERALS.each do |number, letter|
+    result << letter * (val / number)
+    val = val % number
+  end
+  result
+end
+
+def recursive_romanize(val)
+  return "" if val == 0
+  ROMAN_NUMERALS.each do |number, letter|
+    return (letter * (val / number)) << recursive_romanize(val % number) if number <= val
+  end
+end
+
