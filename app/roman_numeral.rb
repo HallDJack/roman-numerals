a2cbd7f4f0d7819dd7690d1bd6a6e232e05f9f92
Jack Hall <jackh@groupon.com>
2014-10-30 09:45:58 -0500

    only the recursive solution

--- app/roman_numeral.rb
+++ app/roman_numeral.rb
@@ -1,31 +1,3 @@
-class Integer
-  ROMAN_NUMERALS = {
-    1000 => "M",
-    900  => "CM",
-    500  => "D",
-    400  => "CD",
-    100  => "C",
-    90   => "XC",
-    50   => "L",
-    40   => "XL",
-    10   => "X",
-    9    => "IX",
-    5    => "V",
-    4    => "IV",
-    1    => "I",
-  }
-
-  def to_roman
-    n = self
-    result = ""
-    ROMAN_NUMERALS.each do |number, letter|
-      result << letter * (n / number)
-      n = n % number
-    end
-    result
-  end
-end
-
 ROMAN_NUMERALS = {
   1000 => "M",
   900  => "CM",
@@ -41,15 +13,6 @@ ROMAN_NUMERALS = {
   4    => "IV",
   1    => "I",
 }
-
-def romanize(val)
-  result = ""
-  ROMAN_NUMERALS.each do |number, letter|
-    result << letter * (val / number)
-    val = val % number
-  end
-  result
-end
 
 def recursive_romanize(val)
   return "" if val == 0
--- spec/roman_numeral_spec.rb
+++ spec/roman_numeral_spec.rb
@@ -1,31 +1,17 @@
 require 'rspec'
 require_relative '../app/roman_numeral.rb'
 
-describe Integer do
-  {
-    1 => "I", 2 => "II", 3 => "III", 4 => "IV", 5 => "V",
-    9 => "IX", 10 => "X", 15 => "XV", 20 => "XX", 44 => "XLIV",
-    98 => "XCVIII", 99 => "XCIX", 100 => "C", 130 => "CXXX",
-    438 => "CDXXXVIII", 499 => "CDXCIX", 527 => "DXXVII",
-    818 => "DCCCXVIII", 900 => "CM", 947 => "CMXLVII",
-    1000 => "M", 1387 => "MCCCLXXXVII", 1500 => "MD"
-  }.each do |value, roman_numeral|
-    describe "to_roman" do
-      it "converts #{value} to #{roman_numeral}" do
-        value.to_roman.should == roman_numeral
-      end
-    end
-
-    describe "#romanize" do
-      it "converts #{value} to #{roman_numeral}" do
-        romanize(value).should == roman_numeral
-      end
-    end
-
-    describe "#recursive_romanize" do
-      it "converts #{value} to #{roman_numeral}" do
-        recursive_romanize(value).should == roman_numeral
-      end
+{
+  1 => "I", 2 => "II", 3 => "III", 4 => "IV", 5 => "V",
+  9 => "IX", 10 => "X", 15 => "XV", 20 => "XX", 44 => "XLIV",
+  98 => "XCVIII", 99 => "XCIX", 100 => "C", 130 => "CXXX",
+  438 => "CDXXXVIII", 499 => "CDXCIX", 527 => "DXXVII",
+  818 => "DCCCXVIII", 900 => "CM", 947 => "CMXLVII",
+  1000 => "M", 1387 => "MCCCLXXXVII", 1500 => "MD"
+}.each do |value, roman_numeral|
+  describe "#recursive_romanize" do
+    it "converts #{value} to #{roman_numeral}" do
+      recursive_romanize(value).should == roman_numeral
     end
   end
 end
